3a6dd1be60c038344c058b90ffeb0779de1d5cef
Claudia <claudiamarzalcri@gmail.com>
2022-03-10 13:59:45 +0100

    Tablero que se imprime y con diccionario

--- tablero.py
+++ tablero.py
@@ -1,14 +1,36 @@
 class Tablero:
-    tablero = []
+    tablero = {}
     letras = ["a", "b", "c", "d", "e", "f", "g", "h"]
     for j in range(8, 0, -1):
-        fila = []
         for letra in letras:
-            fila.append(letra + str(j))
-        tablero.append(fila)
+            tablero[letra + str(j)] = None
 
+    orden_piezas = ["T", "C", "A", "D", "R", "A", "C", "T"]
+    blancas = 0
+    negras = 0
+    for key in tablero:
+        if key[1] == "8":
+            tablero[key] = orden_piezas[negras] + "-N"
+            negras += 1
+        elif key[1] == "1":
+            tablero[key] = orden_piezas[blancas] + "-B"
+            blancas += 1
+        elif key[1] == "7":
+            tablero[key] = "P-N"
+        elif key[1] == "2":
+            tablero[key] = "P-B"
+    
+    def imprimir_tablero(self):
+        for i in range(8, 0, -1):
+            fila = "|"
+            for key in self.tablero:
+                if key[1] == str(i):
+                    elemento = self.tablero[key]
+                    if elemento == None:
+                        elemento = "   "
+                    fila += elemento + "|"
+            print(fila)
 
 
 tablero = Tablero()
-for i in range(8):
-    print(tablero.tablero[i])
+print(tablero.imprimir_tablero())
