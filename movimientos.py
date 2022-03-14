9bccdfb1d8e01c8585ecd78b641b8d9198ae6bbf
GabiMontolio <gabimon04@gmail.com>
2022-03-14 10:44:11 +0100

    Terminar movimiento horizontal

--- movimientos.py
+++ movimientos.py
@@ -1,16 +1,39 @@
-estado_inicial = ""
-letras = ["a","b","c","d","e","f","g","h"]
-tablero = {}
-aliados = tablero[estado_inicial][-1]
-enemigo = "B" if aliados == "N" else "N"
+from tablero import Tablero
+
+inicial,final = "Ta2 a8".split()
+
+tablero = Tablero()
+
+letras = tablero.letras
+
+estado_inicial = inicial[1]+inicial[2]
+estado_final = final
+
+aliado = tablero.tablero[estado_inicial][-1]
+
+enemigo = "B" if aliado == "N" else "N"
+
 columna_inicial = estado_inicial[0]
+
 fila_inicial = estado_inicial[1]
+
 indice_columna_inicial = letras.index(columna_inicial)
 
-def mov_validos_horiz():  
+lista_mov_posibles = []
+def mov_validos_horiz(aliado,enemigo,columna_inicial,fila_inicial_indice_columan_inicial):  
     for i in range(indice_columna_inicial,7,1):
         posicion_comprobando = letras[i+indice_columna_inicial]
-        if tablero[posicion_comprobando][-1] == 
-
+        if tablero.tablero[posicion_comprobando][-1] == aliado:
+            break
+        else:
+            lista_mov_posibles.append(posicion_comprobando)
+    
     for i in range(0,indice_columna_inicial,-1):
-
+        posicion_comprobando = letras[i+indice_columna_inicial]
+        if tablero.tablero[posicion_comprobando][-1] == aliado:
+            break
+        else:
+            lista_mov_posibles.append(posicion_comprobando)
+print(tablero)    
+mov_validos_horiz(aliado,enemigo,columna_inicial,fila_inicial,indice_columna_inicial)
+print(lista_mov_posibles)
